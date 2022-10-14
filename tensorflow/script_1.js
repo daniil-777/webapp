3bdd5bd0c8c31d0d65e799aad436f13ba4da1f1c
daniil-777 <daniil.emtsev.ig@gmail.com>
2022-10-14 12:08:06 +0200

    second commit

--- tensorflow/script_1.js
+++ tensorflow/script_1.js
@@ -2,7 +2,7 @@ const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
 const enableWebcamButton = document.getElementById('webcamButton');
-
+const webcam = new Webcam(document.getElementById('wc'));
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
@@ -81,76 +81,18 @@ var children = [];
 
 
 
-const webcam = new Webcam(document.getElementById('wc'));
-const img_input = webcam.capture();
-
-const input = tf.tidy(() => {
-    const img = tf.browser.fromPixels(img_input)
-    return img.expandDims(0)
-  })
 
 
-
-async function predict() {
-
-  while (True) {
-
-    const img = webcam.capture();
-    model.predict(img).then(function (predictions) {
-      // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
-    
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-
-  });
-  await tf.nextFrame();
-
-}
-}
-  
-
-
-
-  async function init(){
-      await webcam.setup();
-      mobilenet = await loadMobilenet();
-      tf.tidy(() => mobilenet.predict(webcam.capture()));
-        
-    }
-
+// const input = tf.tidy(() => {
+//     const img = tf.browser.fromPixels(img_input)
+//     return img.expandDims(0)
+//   })input
 
 
 function predictWebcam() {
+  const img_input = webcam.capture();
   // Now let's start classifying a frame in the stream.
-  model.predict(input).then(function (predictions) {
+  model.predict(img_input).then(function (predictions) {
     // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
